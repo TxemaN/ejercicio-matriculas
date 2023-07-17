c0ac00d61d34038844147c48140f22a37cdd06e7
TxemaN <txalcala@hotmail.com>
2023-07-17 20:05:30 +0200

    promesa soluconada

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const fraseExisteOMalEscrita = document.querySelector("#fraseExisteOMalEscrita")
 
 const multasAcumuladas = document.querySelector("#multasAcumuladas")
 const form = document.querySelector("#formulario")
+let matriculaIntroducida = document.querySelector("#matriculaIntroducida")
 const fragment = document.createDocumentFragment();
 
 
@@ -13,25 +14,8 @@ let arrayMultas = JSON.parse(localStorage.getItem("comprobadosArray")) || []
 //EVENTO SUBMIT//
 form.addEventListener('submit', (ev) => {
   ev.preventDefault()
-
-
-  const validado = validar();
-  if (!validado) {
-
-    let mensaje = "formato no válido"
-
-    existeOMalEscrita.textContent = mensaje
-  }
-
-  if (validado) {
-   
-    // subirArrayLocal() SUBIR A LOCAL FUNCIONA
-    getConductor()
-    
-  }
-
-  form.reset()
-
+  const matricula = matriculaIntroducida.value
+  verificar(matricula)
 }
 );
 
@@ -41,20 +25,39 @@ const regExp = {
   matricula: /\d\d\d-[A-Za-z]{1}$/i,
 }
 
+const verificar = (matricula) => {
 
-const validar = () => {
+  const validado = validar(matricula);
+  if (!validado) {
 
-  let matricula = document.querySelector("#matriculaIntroducida").value;
+    let mensaje = "formato no válido"
 
-  if (!regExp.matricula.test(matricula)) {
-    return false;
+    existeOMalEscrita.textContent = mensaje
+  } else {
+
+    getInfoConductor(matricula)
+      .then((respuesta) => {
+        multasAcumuladas.innerHTML = respuesta;
+        subirArrayLocal(matricula)
+        pintarMultas(matricula)
+      })
+      .catch((error) => { existeOMalEscrita.innerHTML = error })
+    form.reset()
   }
 
-  else {
+
+
+}
+const validar = (matriculilla) => {
+
+
+
+  if (!regExp.matricula.test(matriculilla)) {
+    return false;
+  } else {
     return true;
   }
 }
-
 
 //ESTO PPARA comprobar si existe//
 
@@ -63,52 +66,47 @@ const arrayConductores = [
   { matricula: "123-b", nombre: 'Ana', multa: 500, modelo: "Canyonero" },
   { matricula: "123-e", nombre: 'bea', multa: 150, modelo: "Deportivo" }
 ];
+const arrayMulta = [
+  { matricula: "123-j", multa: 800 },
+  { matricula: "123-b", multa: 500 },
+  { matricula: "123-e", multa: 150 },
+];
+
+const arrayModelo = [
+  { matricula: "123-j", modelo: "MPajero" },
+  { matricula: "123-b", modelo: "Canyonero" },
+  { matricula: "123-e", modelo: "Deportivo" },
+]
 
 
+const getInfoConductor = async (matricula) => {
+  try {
+    const nombre = arrayConductores.find((item) => item.matricula === matricula)?.nombre
 
 
-let matricula = document.querySelector("#matriculaIntroducida").value;
-
-
-const getConductor=async(matricula)=>{
-  matricula= document.querySelector("#matriculaIntroducida").value;
-  const nombre=arrayConductores.find((item)=>item.matricula==matricula)?.nombre
-	if(nombre) return(`El vehículo con matrícula ${matricula} tiene multa`);
-	else throw(`El vehículo con matrícula ${matricula} no tiene multa`)
-		
-}
-
-
-const getInfoConductor=async(matricula)=>{
-  try{
-    const matriculilla=await getConductor(matricula);
-    
-    
-    return `El vehículo con matrícula ${matriculilla} tiene multa`
-
-  }catch(error){
-throw error
+    if (nombre) return (nombre);
+    else throw (`El vehículo con matrícula ${matricula} no tiene multa`)
+  }
+  catch (error) {
+    throw error
   }
 }
 
-getInfoConductor(matricula)
-.then((respuesta)=>{multasAcumuladas.innerHTML =respuesta;
-  subirArrayLocal ()})
-.catch((error)=>{existeOMalEscrita.innerHTML =error})
+
 
 
 
 //RELLENAR ARRAY LOCAL//
-const subirArrayLocal = (matriculilla) => {
-  matriculilla = document.querySelector("#matriculaIntroducida").value;
-  const coincidencia = arrayMultas.find((elemento) => elemento.matricula == matriculilla)
+const subirArrayLocal = (matricula) => {
+
+  const coincidencia = arrayMultas.find((elemento) => elemento.matricula == matricula)
 
   if (!coincidencia) {
     arrayMultas.push({
-      matricula: matriculilla,
-      nombre: arrayConductores.find((item) => item.matricula == matriculilla)?.nombre,
-      multa: arrayConductores.find((item) => item.matricula == matriculilla)?.multa,
-      modelo: arrayConductores.find((item) => item.matricula == matriculilla)?.modelo
+      matricula: arrayConductores.find((item) => item.matricula == matricula)?.matricula,
+      nombre: arrayConductores.find((item) => item.matricula == matricula)?.nombre,
+      multa: arrayConductores.find((item) => item.matricula == matricula)?.multa,
+      modelo: arrayConductores.find((item) => item.matricula == matricula)?.modelo
     })
 
     localStorage.setItem("comprobadosArray", JSON.stringify(arrayMultas));
@@ -118,13 +116,23 @@ const subirArrayLocal = (matriculilla) => {
 //PINTARLO EN LA WEB
 
 const pintarMultas = () => {
+  let mensaje = ""
+
+  existeOMalEscrita.textContent = mensaje
   multasAcumuladas.innerHTML = ""
   arrayMultas.forEach((item) => {
-    const nuevaMulta = document.createElement("P");
-    nuevaMulta.textContent = `${item.matricula} ${item.modelo} ${item.nombre} ${item.multa}`;
-    fragment.append(nuevaMulta);
+    const nuevalinea = document.createElement("TR");
+    const nuevaMatricula = document.createElement("TD");
+    nuevaMatricula.textContent = `${item.matricula}`;
+    const nuevoModelo = document.createElement("TD");
+    nuevoModelo.textContent = `${item.modelo}`
+    const nuevoNombre = document.createElement("TD");
+    nuevoNombre.textContent = `${item.nombre}`
+    const nuevaMulta = document.createElement("TD");
+    nuevaMulta.textContent = `${item.multa}`
+
+    nuevalinea.append(nuevaMatricula);
+    fragment.append(nuevalinea, nuevaMatricula, nuevoModelo, nuevoNombre, nuevaMulta)
   })
   multasAcumuladas.append(fragment);
 };
-pintarMultas()
-console.log(arrayMultas)
